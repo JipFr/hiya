c11f6917db9f2cfa0ae8d4198f28204e51e037df
Jip Fr <jipfrijlink@gmail.com>
2023-04-07 17:31:28 +0200

    Tree stuff

--- generators/3dmap.js
+++ generators/3dmap.js
@@ -12,7 +12,7 @@ module.exports = () => {
 	const particleSize = 1;
 	const noiseDivider = 5;
 	const offset = 2;
-
+	const waterTreshold = -0.6 * m;
 	const prefix = "^";
 
 	for (let x = 0; x < width; x += steppingSize) {
@@ -24,7 +24,7 @@ module.exports = () => {
 			// let data = [128, 255, 128];
 			const gV = (y / width) * 128 + 128;
 			let data = [0, ((value + 1) / 2) * 200 + 55, 0];
-			let waterTreshold = -0.6 * m;
+
 			if (relYPos < waterTreshold) {
 				data = [128, 128, 255];
 			} else if (relYPos > 0.5 * m) {
@@ -62,6 +62,42 @@ module.exports = () => {
 					`particle minecraft:dust ${rgb} ${particleSize} ${pos} 0 0 0 20 1 force`
 				);
 			}
+		}
+	}
+
+	// Generate trees
+	for (let i = 0; i < 3; i++) {
+		let isAboveWater = false;
+		let x;
+		let y;
+		let relYPos;
+		while (!isAboveWater) {
+			x = Math.floor(Math.random() * width * 100) / 100;
+			y = Math.floor(Math.random() * length * 100) / 100;
+			let value = noise.simplex2(x / noiseDivider, y / noiseDivider);
+			relYPos = Math.floor(value * m * 100) / 100;
+			isAboveWater = relYPos > waterTreshold;
+		}
+
+		const data = [150, 75, 0];
+		const r = data[0] / 255;
+		const g = data[1] / 255;
+		const b = data[2] / 255;
+		const rgb = `${r} ${g} ${b}`;
+
+		let xP = Math.floor(x * 100) / 100;
+		let yP = Math.floor(y * 100) / 100;
+
+		const rX = xP - width / 2;
+		const rY = yP - length / 2;
+
+		for (let t = 0; t < 4; t++) {
+			const pos = `${prefix}${rX} ${prefix}${
+				relYPos + offset + t / 10
+			} ${prefix}${rY}`;
+			commands.push(
+				`particle minecraft:dust ${rgb} 0.3 ${pos} 0 0 0 20 1 force`
+			);
 		}
 	}
 
